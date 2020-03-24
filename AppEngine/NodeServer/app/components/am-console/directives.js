eafa0a6b535dbe498cadfca0987d058f2c228dc8
Tomasz Kunicki <tkunicki@adrem.com.pl>
2020-03-24 03:13:38 +0100

    Update directives.js

--- AppEngine/NodeServer/app/components/am-console/directives.js
+++ AppEngine/NodeServer/app/components/am-console/directives.js
@@ -6,20 +6,21 @@
         .component("amConsole", {
             template: `
                   <h4>Live Report</h4>
-                  <p class="desc">This datalist publishes last 5 console messages from the server.</p>
+                  <p class="desc">This live report publishes last 5 console messages from the server.</p>
                   <ul>
                      <li ng-repeat="rec in $ctrl.dataList.data track by rec.values.Id">{{::rec.values.message}}</li>
                   </ul>
                 `,
             controller: class {
                 constructor($scope) {
-                    this.dataList = new adrem.RemoteDataListStore('srv', 250); // do not refresh faster than 250ms
-                    this.dataList.on('changed', () => $scope.$digest());
-                    this.dataList.open('ConsoleLog', '', (res) => console.log('Opened:', res));
+                    this.dataList = new adrem.RemoteDataListStore('srv', 250); // do not refresh often than every 250ms
+                    this.dataList.on('changed', () => $scope.$digest());       // refresh view
+                    this.dataList.open('ConsoleLog', '', (res) => console.log('Opened and read: ', res.length));
                 }
 
                 $onDestroy() {
-                    this.dataList.finalize();
+                    // this will close datalist and cleanup remote instance
+                    // we can't rely on GC
                     this.dataList.destroy();
                 }
             }
